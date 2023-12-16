e9ce5517760cd41e78ace1d6e2866ed2e57ca2ba
Carl Andersson <carl@carlandersson.net>
2023-12-16 08:12:58 +0100

    Improve Day 5 - Use range math for second part

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -1,17 +1,62 @@
+use std::ops::Range;
+
+use crate::helpers::RangeIntersect;
+
 use super::day::Day;
 use anyhow::Result;
-use rayon::prelude::*;
+use itertools::Itertools;
+use smallvec::{smallvec, SmallVec};
 
 #[derive(Debug, Clone)]
 pub struct Map {
-    destination_start: i64,
-    source_start: i64,
-    range_length: i64,
+    range: Range<i64>,
+    destination: i64,
 }
 #[derive(Debug, Clone)]
 pub struct Almanac {
     seeds: Vec<i64>,
     maps_list: Vec<Vec<Map>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Ranger {
+    ranges: SmallVec<[Range<i64>; 32]>,
+}
+impl Ranger {
+    fn new(range: Range<i64>) -> Self {
+        Self {
+            ranges: smallvec![range],
+        }
+    }
+    fn apply_maps(&mut self, maps: &Vec<Map>) {
+        let mut finished_ranges: SmallVec<[_; 32]> = SmallVec::new();
+        for map in maps {
+            let mut outside_ranges: SmallVec<[_; 32]> = SmallVec::new();
+            while let Some(range) = self.ranges.pop() {
+                // Is the range to the "left" of the map
+                if range.start < map.range.start {
+                    // Add part of the range that is to the "left" of the map
+                    outside_ranges.push(range.start..range.end.min(map.range.start));
+                }
+                // Does the range intersect with the map
+                if let Some(intersect) = map.range.intersect(&range) {
+                    // The interecting range is changed according to the map
+                    let diff = map.destination - map.range.start;
+                    finished_ranges.push(intersect.start + diff..intersect.end + diff);
+                }
+                // Is the range to the "right" of the map
+                if range.end > map.range.end {
+                    // Add part of the range that is to the "right" of the map
+                    outside_ranges.push(map.range.end.max(range.start)..range.end);
+                }
+            }
+            self.ranges = outside_ranges;
+        }
+        self.ranges.append(&mut finished_ranges);
+    }
+    fn min(&self) -> i64 {
+        self.ranges.iter().map(|r| r.start).min().unwrap()
+    }
 }
 
 pub struct Day5;
@@ -22,65 +67,57 @@ impl Day for Day5 {
     fn parse(input: String) -> Result<Self::Parsed> {
         let (seeds, maps) = input.split_once("\n\n").unwrap();
         let seeds = seeds.split(' ').filter_map(|s| s.parse().ok()).collect();
-        let maps = maps
+        let maps_list = maps
             .split("\n\n")
             .map(|map| {
                 map.lines()
                     .skip(1)
-                    .map(|line| {
-                        let mut nums = line.split(' ').map(|num| num.parse::<i64>().unwrap());
-                        Map {
-                            destination_start: nums.next().unwrap(),
-                            source_start: nums.next().unwrap(),
-                            range_length: nums.next().unwrap(),
-                        }
+                    .filter_map(|line| {
+                        let nums: (i64, i64, i64) = line
+                            .split(' ')
+                            .map(|s| s.parse::<i64>().unwrap())
+                            .collect_tuple()?;
+                        Some(Map {
+                            range: nums.1..(nums.1 + nums.2),
+                            destination: nums.0,
+                        })
                     })
                     .collect()
             })
             .collect();
-        Ok(Almanac { seeds, maps_list: maps })
+        Ok(Almanac { seeds, maps_list })
     }
     fn first(almanac: Self::Parsed) -> Self::Output {
-        let mut lowest = i64::MAX;
-        for mut seed in almanac.seeds {
-            for maps in &almanac.maps_list {
-                for map in maps {
-                    let range = map.source_start..(map.source_start+map.range_length);
-                    if range.contains(&seed) {
-                        seed += map.destination_start - map.source_start;
-                        break;
-                    }
-                }
-            }
-            if seed < lowest {
-                lowest = seed;
-            }
-        }
-        lowest
-    }
-    fn second(almanac: Self::Parsed) -> Self::Output {
-        let ranges: Vec<[i64;2]> = almanac.seeds.chunks_exact(2).map(|a| [a[0], a[1]]).collect();
-        ranges.into_par_iter().map(|seed_range| {
-            println!("Testing seed range: {seed_range:?}");
-            let mut lowest = i64::MAX;
-            for mut seed in seed_range[0]..(seed_range[0]+seed_range[1]) {
-
+        almanac
+            .seeds
+            .into_iter()
+            .map(|mut seed| {
                 for maps in &almanac.maps_list {
                     for map in maps {
-                        let range = map.source_start..(map.source_start+map.range_length);
-                        if range.contains(&seed) {
-                            seed += map.destination_start - map.source_start;
+                        if map.range.contains(&seed) {
+                            seed += map.destination - map.range.start;
                             break;
                         }
                     }
                 }
-                if seed < lowest {
-                    lowest = seed;
+                seed
+            })
+            .min()
+            .unwrap()
+    }
+    fn second(almanac: Self::Parsed) -> Self::Output {
+        almanac
+            .seeds
+            .chunks_exact(2)
+            .map(|chunk| {
+                let mut ranger = Ranger::new(chunk[0]..chunk[0] + chunk[1]);
+                for maps in &almanac.maps_list {
+                    ranger.apply_maps(maps);
                 }
-            }
-            println!("Lowest: {lowest:?}");
-            lowest
-        }).min().unwrap()
+                ranger.min()
+            })
+            .min()
+            .unwrap()
     }
 }
 
